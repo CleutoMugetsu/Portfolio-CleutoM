f82740fd9f621ce01d3d7961c787e89fed2bfe23
CleutoMugetsu <cleutojunior@hotmail.com>
2024-03-13 08:36:39 -0300

    atualização de linguagens

--- src/data/tools.tsx
+++ src/data/tools.tsx
@@ -1,9 +1,9 @@
 export const tools = [
   {id: 1, name: "HTML", img: "/html.svg" },
   {id: 2, name: "CSS", img: "/css.svg" },
-  {id: 3, name: "Javascript", img: "/js.svg" },
-  {id: 4, name: "Typescript", img:"/typescript2-white.png" },
+  {id: 4, name: "Typescript", img:"/typescript2.svg" },
   {id: 5, name: "Node", img: "/node.svg" },
-  {id: 6, name: "React", img: "/react-white.png" },
+  {id: 6, name: "React", img: "/react.svg" },
   {id: 7, name: "Postgresql", img:"/postgresql.svg"},
+  {id: 8, name: "Python", img:"/python.png"},
 ];
